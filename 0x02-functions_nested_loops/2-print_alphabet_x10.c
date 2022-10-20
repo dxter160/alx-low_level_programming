ccbe98fc6a7409424511475bf9aac21154419db8
dxter160 <clintonugochukwu160@gmail.com>
2022-10-19 17:03:13 -0700

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * print_alphabet - prints all alphabets in lowercase
+ * return on success 1
+ */
+
+int main(void)
+{
+	char alphabet;
+	alphabet= 'a';
+	_putchar("Lowercase alphabets:\n");
+	while(alphabet<='z')
+	{
+		_putchar("%c ",alphabet);
+		alphabet++
+
+
+
+	return (0);
+
+}
+
