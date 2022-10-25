ff9c7d78e82b36b7c0b72f8bbe3c479275910883
dxter160 <clintonugochukwu160@gmail.com>
2022-10-24 19:34:53 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i--)
+
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
