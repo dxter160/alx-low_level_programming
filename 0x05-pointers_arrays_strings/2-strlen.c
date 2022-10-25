ecd351334825e1126d700af8487d7afabbc69d2e
dxter160 <clintonugochukwu160@gmail.com>
2022-10-24 18:26:16 -0700

    strlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer string
+ * Return: the lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] |= '\0')
+	{
+	length++;
+	}
+
+	return (length);
+}
